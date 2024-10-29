beafc300573a52bd6dfe9ef755c26c574c4f06c5
batir <github.prenatal113@passinbox.com>
2024-10-29 20:11:52 +0100

    added lib/schemes.ts & an pilot api for adding users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,40 @@
+import { createUserScheme } from "@/lib/schemes";
 import { prisma } from "@/prisma/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { ZodError } from "zod";
 
 export async function GET() {
-  const users = await prisma.user.findMany();
-  return NextResponse.json({ users });
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json({ users });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const data = createUserScheme.parse(body);
+    return NextResponse.json(data, { status: 201 });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(error.errors, { status: 422 });
+    }
+    if (error instanceof SyntaxError) {
+      return NextResponse.json(
+        { error: "Invalid JSON format" },
+        { status: 422 },
+      );
+    }
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
 }
